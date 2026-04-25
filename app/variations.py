16e595ecbbe4d43e2778f64c792ee44ed4f7e9e7
namanpateriya <pateriyanaman@gmail.com>
2026-04-25 12:48:06 +0530

    Create variations.py

---
+++ app/variations.py
@@ -0,0 +1,6 @@
+VARIATIONS = {
+    "concise": "Explain in 2-3 sentences:\n{input}",
+    "detailed": "Explain in detail:\n{input}",
+    "bullet_points": "Explain in bullet points:\n{input}",
+    "beginner": "Explain like I'm a beginner:\n{input}"
+}
